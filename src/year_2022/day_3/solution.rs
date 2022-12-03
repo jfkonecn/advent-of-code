b25bb9bfcf3c288dfc281a4b2bc8bce67451eda4
John <jfkonecn@gmail.com>
2022-12-03 11:58:56 -0500

    a

--- src/year_2022/day_3/solution.rs
+++ src/year_2022/day_3/solution.rs
@@ -1,4 +1,5 @@
 use crate::common::inputs::challenge_test_suite;
+use itertools::Itertools;
 use std::collections::HashSet;
 
 pub fn solution_1(file_contents: String) -> usize {
@@ -20,42 +21,51 @@ pub fn solution_1(file_contents: String) -> usize {
             }
             panic!("no matches between \"{}\" and \"{}\"", a, b)
         })
-        .map(|c| {
-            let small_a = 'a' as usize;
-            let big_a = 'A' as usize;
-            let char_num = c as usize;
-            let num = if char_num >= small_a {
-                char_num - small_a + 1
-            } else {
-                char_num - big_a + 27
-            };
-            num
-        })
+        .map(char_to_value)
         .sum()
 }
 
+fn char_to_value(c: char) -> usize {
+    let small_a = 'a' as usize;
+    let big_a = 'A' as usize;
+    let char_num = c as usize;
+    let num = if char_num >= small_a {
+        char_num - small_a + 1
+    } else {
+        char_num - big_a + 27
+    };
+    num
+}
+
 pub fn solution_2(file_contents: String) -> usize {
-    1
-    // file_contents
-    //     .split("\n")
-    //     .filter(|x| !x.is_empty())
-    //     .map(|str| {
-    //         let arr = str.split(' ').collect::<Vec<&str>>();
-    //         let opponent: Rps = (*arr.get(0).unwrap()).into();
-    //         let winner: RpsWinner = (*arr.get(1).unwrap()).into();
-    //         let me = should_pick(&opponent, &winner);
-    //         usize::from(winner) + usize::from(me)
-    //     })
-    //     .sum()
+    file_contents
+        .split("\n")
+        .filter(|x| !x.is_empty())
+        .enumerate()
+        .group_by(|(idx, _)| idx / 3)
+        .into_iter()
+        .map(|(_, group)| {
+            group
+                .into_iter()
+                .map(|(_, x)| x)
+                .flat_map(|x| x.chars().map(char_to_value).unique())
+                .counts()
+                .into_iter()
+                .filter(|(_, x)| *x == 3usize)
+                .map(|(x, _)| x)
+                .next()
+                .unwrap()
+        })
+        .sum()
 }
 
 challenge_test_suite!(
     solution_1,
     157,
-    1,
-    // solution_2,
-    // 12,
-    // 11373,
+    8394,
+    solution_2,
+    70,
+    2413,
     "src",
     "year_2022",
     "day_3"
