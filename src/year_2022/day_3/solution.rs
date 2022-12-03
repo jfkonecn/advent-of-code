1a0dc47793da68c94287cbd275e8de2d73cd2536
John <jfkonecn@gmail.com>
2022-12-03 11:07:04 -0500

    a

--- src/year_2022/day_3/solution.rs
+++ src/year_2022/day_3/solution.rs
@@ -1,123 +1,62 @@
 use crate::common::inputs::challenge_test_suite;
-
-enum Rps {
-    Rock,
-    Paper,
-    Scissors,
-}
-
-impl From<&str> for Rps {
-    fn from(str: &str) -> Self {
-        match str {
-            "A" | "X" => Rps::Rock,
-            "B" | "Y" => Rps::Paper,
-            "C" | "Z" => Rps::Scissors,
-            _ => panic!("unknown char \"{}\"", str),
-        }
-    }
-}
-
-impl From<Rps> for usize {
-    fn from(rps: Rps) -> Self {
-        match rps {
-            Rps::Rock => 1,
-            Rps::Paper => 2,
-            Rps::Scissors => 3,
-        }
-    }
-}
-
-enum RpsWinner {
-    Me,
-    Opponent,
-    Draw,
-}
-
-fn check_winner(me: &Rps, opponent: &Rps) -> RpsWinner {
-    match (me, opponent) {
-        (Rps::Rock, Rps::Scissors) | (Rps::Paper, Rps::Rock) | (Rps::Scissors, Rps::Paper) => {
-            RpsWinner::Me
-        }
-        (Rps::Scissors, Rps::Rock) | (Rps::Rock, Rps::Paper) | (Rps::Paper, Rps::Scissors) => {
-            RpsWinner::Opponent
-        }
-        (Rps::Scissors, Rps::Scissors) | (Rps::Rock, Rps::Rock) | (Rps::Paper, Rps::Paper) => {
-            RpsWinner::Draw
-        }
-    }
-}
-
-impl From<&str> for RpsWinner {
-    fn from(str: &str) -> Self {
-        match str {
-            "X" => RpsWinner::Opponent,
-            "Y" => RpsWinner::Draw,
-            "Z" => RpsWinner::Me,
-            _ => panic!("unknown char \"{}\"", str),
-        }
-    }
-}
-
-fn should_pick(opponent: &Rps, winner: &RpsWinner) -> Rps {
-    match (opponent, winner) {
-        (Rps::Rock, RpsWinner::Draw)
-        | (Rps::Paper, RpsWinner::Opponent)
-        | (Rps::Scissors, RpsWinner::Me) => Rps::Rock,
-        (Rps::Paper, RpsWinner::Draw)
-        | (Rps::Scissors, RpsWinner::Opponent)
-        | (Rps::Rock, RpsWinner::Me) => Rps::Paper,
-        (Rps::Scissors, RpsWinner::Draw)
-        | (Rps::Rock, RpsWinner::Opponent)
-        | (Rps::Paper, RpsWinner::Me) => Rps::Scissors,
-    }
-}
-
-impl From<RpsWinner> for usize {
-    fn from(winner: RpsWinner) -> Self {
-        match winner {
-            RpsWinner::Opponent => 0,
-            RpsWinner::Draw => 3,
-            RpsWinner::Me => 6,
-        }
-    }
-}
+use std::collections::HashSet;
 
 pub fn solution_1(file_contents: String) -> usize {
     file_contents
         .split("\n")
         .filter(|x| !x.is_empty())
         .map(|str| {
-            let arr = str.split(' ').collect::<Vec<&str>>();
-            let opponent: Rps = (*arr.get(0).unwrap()).into();
-            let me: Rps = (*arr.get(1).unwrap()).into();
-            let winner = check_winner(&me, &opponent);
-            usize::from(winner) + usize::from(me)
+            let idx = str.len() / 2;
+            let a = &str[..idx];
+            let b = &str[idx..];
+            let mut set = HashSet::new();
+            for c in a.chars() {
+                set.insert(c);
+            }
+            for c in b.chars() {
+                if set.contains(&c) {
+                    return c;
+                }
+            }
+            panic!("no matches between \"{}\" and \"{}\"", a, b)
+        })
+        .map(|c| {
+            let small_a = 'a' as usize;
+            let big_a = 'A' as usize;
+            let char_num = c as usize;
+            let num = if char_num >= small_a {
+                char_num - small_a + 1
+            } else {
+                char_num - big_a + 27
+            };
+            num
         })
         .sum()
 }
 
 pub fn solution_2(file_contents: String) -> usize {
-    file_contents
-        .split("\n")
-        .filter(|x| !x.is_empty())
-        .map(|str| {
-            let arr = str.split(' ').collect::<Vec<&str>>();
-            let opponent: Rps = (*arr.get(0).unwrap()).into();
-            let winner: RpsWinner = (*arr.get(1).unwrap()).into();
-            let me = should_pick(&opponent, &winner);
-            usize::from(winner) + usize::from(me)
-        })
-        .sum()
+    1
+    // file_contents
+    //     .split("\n")
+    //     .filter(|x| !x.is_empty())
+    //     .map(|str| {
+    //         let arr = str.split(' ').collect::<Vec<&str>>();
+    //         let opponent: Rps = (*arr.get(0).unwrap()).into();
+    //         let winner: RpsWinner = (*arr.get(1).unwrap()).into();
+    //         let me = should_pick(&opponent, &winner);
+    //         usize::from(winner) + usize::from(me)
+    //     })
+    //     .sum()
 }
 
 challenge_test_suite!(
     solution_1,
-    15,
-    13005,
-    solution_2,
-    12,
-    11373,
+    157,
+    1,
+    // solution_2,
+    // 12,
+    // 11373,
     "src",
     "year_2022",
-    "day_2"
+    "day_3"
 );
