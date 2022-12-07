56f5e0ddc5d0256b85728e029d8d97db0573a350
John <jfkonecn@gmail.com>
2022-12-07 18:03:35 -0500

    a

--- src/year_2022/day_7/solution.rs
+++ src/year_2022/day_7/solution.rs
@@ -1,11 +1,21 @@
 use crate::common::inputs::challenge_test_suite;
 use itertools::Itertools;
-use std::{collections::VecDeque, iter::Map, str::Split};
+use std::{
+    collections::{HashMap, VecDeque},
+    iter::Map,
+    str::Split,
+};
 
 #[derive(Debug, Clone)]
 enum ParsedFile {
     File { name: String, size: usize },
     Directory(String),
+}
+
+#[derive(Debug, Clone)]
+enum FileTree {
+    File { name: String, size: usize },
+    Directory { name: String, files: Vec<FileTree> },
 }
 
 #[derive(Debug, Clone)]
@@ -56,9 +66,62 @@ impl<'a> From<&str> for Command {
     }
 }
 
+fn build_file_tree(map: &HashMap<String, Vec<ParsedFile>>, current_path: String) -> FileTree {
+    println!("{}", current_path);
+    let cur_dir = map.get(&current_path).unwrap();
+    let files = cur_dir
+        .into_iter()
+        .map(|x| -> FileTree {
+            match x {
+                ParsedFile::File { name, size } => FileTree::File {
+                    name: name.clone(),
+                    size: size.clone(),
+                },
+                ParsedFile::Directory(x) => build_file_tree(map, format!("{}{}/", current_path, x)),
+            }
+        })
+        .collect_vec();
+    FileTree::Directory {
+        name: current_path,
+        files,
+    }
+}
+
 pub fn solution_1(file_contents: String) -> usize {
     let commands = get_commands(&file_contents).collect_vec();
-    println!("{:?}", commands);
+    // path.push("".to_owned());
+    // path.push("a".to_owned());
+    // path.push("c".to_owned());
+    // println!("{:?}", path.join("/"));
+    // path.pop();
+    // println!("{:?}", path.join("/"));
+    let path_map = {
+        let mut path: Vec<String> = vec![];
+        let mut path_map = HashMap::new();
+        commands.iter().for_each(|command| match command {
+            Command::MoveToRoot => {
+                path.clear();
+                path.push("".to_owned());
+            }
+            Command::MoveToParentFolder => {
+                path.pop();
+            }
+            Command::MoveToFolder(dir) => {
+                path.push(dir.to_owned());
+            }
+            Command::ListDir(contents) => {
+                let path = format!("{}/", path.join("/"));
+                path_map.insert(path, contents.clone());
+            }
+        });
+        path_map
+    };
+
+    println!("{:?}", path_map);
+    let file_tree = build_file_tree(&path_map, "/".to_owned());
+
+    // println!("{:?}", commands);
+    println!("{:?}", file_tree);
     1
 }
 
