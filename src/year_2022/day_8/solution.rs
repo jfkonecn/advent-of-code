e1fc714d5898c435101a805e2b80cca2a3ee2606
John <jfkonecn@gmail.com>
2022-12-09 18:18:33 -0500

    a

--- src/year_2022/day_8/solution.rs
+++ src/year_2022/day_8/solution.rs
@@ -1,178 +1,84 @@
 use crate::common::inputs::challenge_test_suite;
 use itertools::Itertools;
-use std::{
-    collections::{HashMap, VecDeque},
-    iter::Map,
-    str::Split,
-};
+use std::{collections::*, iter::Map, str::Split};
 
-#[derive(Debug, Clone)]
-enum ParsedFile {
-    File { name: String, size: usize },
-    Directory(String),
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Copy)]
+struct Tree {
+    row: usize,
+    col: usize,
+    height: usize,
 }
 
-#[derive(Debug, Clone)]
-enum FileTree {
-    File { name: String, size: usize },
-    Directory { name: String, files: Vec<FileTree> },
-}
-
-#[derive(Debug, Clone)]
-enum Command {
-    MoveToRoot,
-    MoveToParentFolder,
-    MoveToFolder(String),
-    ListDir(Vec<ParsedFile>),
-}
-
-fn get_commands<'a>(str: &'a String) -> impl Iterator<Item = Command> + 'a {
-    str.split('$').filter(|x| !x.is_empty()).map(Command::from)
-}
-
-impl<'a> From<&str> for Command {
-    fn from(str: &str) -> Self {
-        let str = str.to_owned();
-        let mut raw_strs = str.split_ascii_whitespace().filter(|x| !x.is_empty());
-        let cmd_str = raw_strs.next().unwrap();
-
-        match cmd_str {
-            "cd" => {
-                let dir = raw_strs.next().unwrap();
-                match dir {
-                    "/" => Command::MoveToRoot,
-                    ".." => Command::MoveToParentFolder,
-                    _ => Command::MoveToFolder(dir.to_owned()),
-                }
-            }
-            "ls" => {
-                let mut files: Vec<ParsedFile> = vec![];
-                while let Some(first) = raw_strs.next() {
-                    let second = raw_strs.next().unwrap();
-                    let parsed_file = if first == "dir" {
-                        ParsedFile::Directory(second.to_owned())
-                    } else {
-                        ParsedFile::File {
-                            name: second.to_owned(),
-                            size: first.parse().unwrap(),
-                        }
-                    };
-                    files.push(parsed_file);
-                }
-                Command::ListDir(files)
-            }
-            _ => unreachable!("Unknown command {}", cmd_str),
-        }
-    }
-}
-
-fn build_file_tree_rec(map: &HashMap<String, Vec<ParsedFile>>, current_path: String) -> FileTree {
-    let cur_dir = map.get(&current_path).unwrap();
-    let files = cur_dir
-        .into_iter()
-        .map(|x| -> FileTree {
-            match x {
-                ParsedFile::File { name, size } => FileTree::File {
-                    name: name.clone(),
-                    size: size.clone(),
-                },
-                ParsedFile::Directory(x) => {
-                    build_file_tree_rec(map, format!("{}{}/", current_path, x))
-                }
-            }
+fn parse_file(file_contents: String) -> Vec<Tree> {
+    file_contents
+        .split('\n')
+        .filter(|x| !x.is_empty())
+        .enumerate()
+        .flat_map(|(row, str)| {
+            str.chars().enumerate().map(move |(col, c)| Tree {
+                height: String::from(c).parse::<usize>().unwrap(),
+                col,
+                row,
+            })
         })
-        .collect_vec();
-    FileTree::Directory {
-        name: current_path,
-        files,
-    }
-}
-fn build_file_tree(file_contents: String) -> FileTree {
-    let commands = get_commands(&file_contents).collect_vec();
-    let path_map = {
-        let mut path: Vec<String> = vec![];
-        let mut path_map = HashMap::new();
-        commands.iter().for_each(|command| match command {
-            Command::MoveToRoot => {
-                path.clear();
-                path.push("".to_owned());
-            }
-            Command::MoveToParentFolder => {
-                path.pop();
-            }
-            Command::MoveToFolder(dir) => {
-                path.push(dir.to_owned());
-            }
-            Command::ListDir(contents) => {
-                let path = format!("{}/", path.join("/"));
-                path_map.insert(path, contents.clone());
-            }
-        });
-        path_map
-    };
-
-    build_file_tree_rec(&path_map, "/".to_owned())
+        .collect_vec()
 }
 
-fn get_dir_sizes_rec(
-    file_trees: &Vec<FileTree>,
-    dir_name: String,
-) -> (Vec<(String, usize)>, usize) {
-    let mut vec: Vec<(String, usize)> = vec![];
-    let mut total: usize = 0;
-    file_trees.iter().for_each(|file_tree| {
-        match file_tree {
-            FileTree::File { size, name } => {
-                total += size;
-            }
-            FileTree::Directory { name, files } => {
-                let (mut sizes, sum) = get_dir_sizes_rec(files, name.clone());
-                total += sum;
-                vec.append(&mut sizes);
-            }
-        };
-    });
-    vec.push((dir_name, total));
-    (vec, total)
-}
-fn get_dir_sizes<'a>(file_tree: FileTree) -> Vec<(String, usize)> {
-    match file_tree {
-        FileTree::Directory { name, files } => {
-            let (vec, _) = get_dir_sizes_rec(&files, name);
-            vec
-        }
-        _ => panic!("unsupported"),
+fn insert_range<T: std::hash::Hash + Eq>(set: &mut HashSet<T>, iter: impl Iterator<Item = T>) {
+    for x in iter {
+        set.insert(x);
     }
+}
+
+fn visible_trees(vec: &Vec<Tree>) -> impl Iterator<Item = Tree> + '_ {
+    let to_take = vec
+        .iter()
+        .zip(vec.iter().skip(1))
+        .take_while(|(l, r)| l.height < r.height)
+        .count();
+    vec.into_iter().take(to_take + 1).map(|x| x.clone())
+}
+
+fn visible_from_outside(trees: &Vec<Tree>) -> Vec<Tree> {
+    let mut set: HashSet<Tree> = HashSet::new();
+    let row_indexes = trees.iter().map(|x| x.row).unique();
+    for row in row_indexes {
+        add_visible(trees, &mut set, &|x: &Tree| x.row == row);
+    }
+    let col_indexes = trees.iter().map(|x| x.col).unique();
+    for col in col_indexes {
+        add_visible(trees, &mut set, &|x: &Tree| x.col == col);
+    }
+    Vec::from_iter(set)
+}
+
+fn add_visible(trees: &Vec<Tree>, set: &mut HashSet<Tree>, filter: &dyn Fn(&Tree) -> bool) {
+    let vec = trees.into_iter().map(|x| *x).filter(filter).collect_vec();
+    insert_range(set, visible_trees(&vec));
+    insert_range(set, visible_trees(&vec.into_iter().rev().collect_vec()));
 }
 
 pub fn solution_1(file_contents: String) -> usize {
-    let file_tree = build_file_tree(file_contents);
-    let sizes = get_dir_sizes(file_tree);
-    sizes
-        .iter()
-        .map(|(_, x)| *x)
-        .filter(|x| x.clone() <= 100000usize)
-        .sum()
+    let trees = parse_file(file_contents);
+    let visible = visible_from_outside(&trees);
+    // println!("{:?}", trees);
+    let mut deb = visible
+        .clone()
+        .into_iter()
+        .map(|x| (x.row, x.col, x.height))
+        .collect_vec();
+    deb.sort();
+    println!("{:?}", deb);
+    visible.len()
 }
 
 pub fn solution_2(file_contents: String) -> usize {
-    let file_tree = build_file_tree(file_contents);
-    let sizes = get_dir_sizes(file_tree);
-    let sizes = sizes.iter().map(|(_, x)| *x).collect_vec();
-    let space_taken = sizes.iter().max().unwrap();
-    let space_left = 70000000 - space_taken;
-    let min_to_delete = 30000000 - space_left;
-    sizes
-        .iter()
-        .map(|x| x.clone())
-        .filter(|x| (*x).clone() >= min_to_delete)
-        .min()
-        .unwrap()
+    1
 }
 
 challenge_test_suite!(
     solution_1,
-    1,
+    21,
     1,
     solution_2,
     1,
