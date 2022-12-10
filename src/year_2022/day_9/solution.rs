a6f71fa2e18ce4bfd9aa6ed628f73ab0dcdb5412
John <jfkonecn@gmail.com>
2022-12-10 10:48:40 -0500

    a

--- src/year_2022/day_9/solution.rs
+++ src/year_2022/day_9/solution.rs
@@ -1,6 +1,6 @@
 use crate::common::inputs::challenge_test_suite;
 use itertools::Itertools;
-use std::collections::*;
+use std::{borrow::Borrow, collections::*};
 
 #[derive(Debug, Clone)]
 enum Direction {
@@ -47,13 +47,15 @@ fn parse_commands(file_contents: String) -> Vec<Command> {
         .collect_vec()
 }
 
-fn simulate(commands: Vec<Command>) -> Vec<Point> {
+fn simulate(commands: Vec<Command>, rope_size: usize) -> Vec<Point> {
     let mut visited = HashSet::new();
     let mut head = Point { x: 0, y: 0 };
-    let mut tail = Point { x: 0, y: 0 };
-    visited.insert(tail.clone());
+
+    let mut rest = (1..rope_size).map(|_| Point { x: 0, y: 0 }).collect_vec();
+    visited.insert(head.clone());
 
     for Command { amount, direction } in commands {
+        println!("{:?}", direction);
         for _ in 0..amount {
             match direction {
                 Direction::Up => {
@@ -69,50 +71,62 @@ fn simulate(commands: Vec<Command>) -> Vec<Point> {
                     head.x += 1;
                 }
             };
-            if head.y == tail.y && head.x > tail.x + 1 {
-                tail.x += 1;
-            } else if head.y == tail.y && head.x < tail.x - 1 {
-                tail.x -= 1;
-            } else if head.x == tail.x && head.y > tail.y + 1 {
-                tail.y += 1;
-            } else if head.x == tail.x && head.y < tail.y - 1 {
-                tail.y -= 1;
-            } else if (head.x == tail.x - 1 && head.y == tail.y - 2)
-                || (head.x == tail.x - 2 && head.y == tail.y - 1)
-            {
-                tail.x -= 1;
-                tail.y -= 1;
-            } else if (head.x == tail.x + 1 && head.y == tail.y + 2)
-                || (head.x == tail.x + 2 && head.y == tail.y + 1)
-            {
-                tail.x += 1;
-                tail.y += 1;
-            } else if (head.x == tail.x - 1 && head.y == tail.y + 2)
-                || (head.x == tail.x - 2 && head.y == tail.y + 1)
-            {
-                tail.x -= 1;
-                tail.y += 1;
-            } else if (head.x == tail.x + 1 && head.y == tail.y - 2)
-                || (head.x == tail.x + 2 && head.y == tail.y - 1)
-            {
-                tail.x += 1;
-                tail.y -= 1;
+            let mut prev = &head;
+            for mut tail in rest.iter_mut() {
+                move_tail(prev, &mut tail);
+                prev = tail;
             }
-            visited.insert(tail.clone());
+            visited.insert(prev.clone());
+            println!("{:?} - {:?}", head, rest);
         }
     }
 
     Vec::from_iter(visited)
 }
 
+fn move_tail(head: &Point, tail: &mut Point) {
+    // println!("({:?}, {:?})", head, tail);
+    if head.y == tail.y && head.x == tail.x + 2 {
+        tail.x += 1;
+    } else if head.y == tail.y && head.x == tail.x - 2 {
+        tail.x -= 1;
+    } else if head.x == tail.x && head.y == tail.y + 2 {
+        tail.y += 1;
+    } else if head.x == tail.x && head.y == tail.y - 2 {
+        tail.y -= 1;
+    } else if (head.x == tail.x - 1 && head.y == tail.y - 2)
+        || (head.x == tail.x - 2 && head.y == tail.y - 1)
+    {
+        tail.x -= 1;
+        tail.y -= 1;
+    } else if (head.x == tail.x + 1 && head.y == tail.y + 2)
+        || (head.x == tail.x + 2 && head.y == tail.y + 1)
+    {
+        tail.x += 1;
+        tail.y += 1;
+    } else if (head.x == tail.x - 1 && head.y == tail.y + 2)
+        || (head.x == tail.x - 2 && head.y == tail.y + 1)
+    {
+        tail.x -= 1;
+        tail.y += 1;
+    } else if (head.x == tail.x + 1 && head.y == tail.y - 2)
+        || (head.x == tail.x + 2 && head.y == tail.y - 1)
+    {
+        tail.x += 1;
+        tail.y -= 1;
+    }
+}
+
 pub fn solution_1(file_contents: String) -> usize {
     let commands = parse_commands(file_contents);
-    let vec = simulate(commands);
+    let vec = simulate(commands, 2);
     vec.len()
 }
 
 pub fn solution_2(file_contents: String) -> usize {
-    1
+    let commands = parse_commands(file_contents);
+    let vec = simulate(commands, 10);
+    vec.len()
 }
 
 challenge_test_suite!(
@@ -121,6 +135,9 @@ challenge_test_suite!(
     6337,
     solution_2,
     1,
+    // 16 is wrong
+    // 13 is wrong
+    // 1 is wrong
     1,
     "src",
     "year_2022",
