562bbc139894725688ec2999de7d123cbd43ef55
John <jfkonecn@gmail.com>
2022-12-13 17:40:23 -0500

    a

--- src/year_2022/day_13/solution.rs
+++ src/year_2022/day_13/solution.rs
@@ -2,6 +2,7 @@ use crate::common::inputs::challenge_test_suite;
 use itertools::Itertools;
 use std::cmp::*;
 use std::collections::*;
+use std::fmt::format;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct PacketList {
@@ -16,19 +17,19 @@ enum PacketIndex {
 
 impl From<&str> for PacketList {
     fn from(str: &str) -> Self {
-        let list = vec![];
-        let mut stack = vec![];
-        let char_iter = str.chars();
+        let mut list = PacketList { list: vec![] };
+        let mut stack: Vec<PacketList> = vec![];
+        let mut char_iter = str.chars();
         while let Some(c) = char_iter.next() {
             let c = if c.is_numeric() {
-                let str = c.to_string().to_owned();
+                let mut str = c.to_string().to_owned();
                 loop {
                     let c = char_iter.next().unwrap();
                     if c.is_numeric() {
                         str.push(c);
                     } else {
                         let num: usize = str.parse().unwrap();
-                        stack.last().unwrap().push(PacketIndex::Num(num));
+                        stack.last_mut().unwrap().list.push(PacketIndex::Num(num));
                         break c;
                     }
                 }
@@ -36,21 +37,24 @@ impl From<&str> for PacketList {
                 c
             };
             if c == '[' {
-                stack.push(vec![]);
+                stack.push(PacketList { list: vec![] });
             } else if c == ']' {
-                list.push(PacketIndex::List(PacketList {
-                    list: stack.pop().unwrap(),
-                }));
+                let cur_list = stack.pop().unwrap();
+                if let Some(pre_list) = stack.last_mut() {
+                    pre_list.list.push(PacketIndex::List(cur_list));
+                } else {
+                    list = cur_list;
+                }
             }
         }
-        PacketList { list }
+        list
     }
 }
 
 fn parse_packets(str: String) -> Vec<(PacketList, PacketList)> {
     str.split("\n\n")
         .map(|x| {
-            let iter = x.split('\n');
+            let mut iter = x.split('\n');
             let first = iter.next().unwrap().into();
             let second = iter.next().unwrap().into();
             (first, second)
@@ -60,7 +64,12 @@ fn parse_packets(str: String) -> Vec<(PacketList, PacketList)> {
 
 pub fn solution_1(file_contents: String) -> usize {
     let packets = parse_packets(file_contents);
-    println!("{:#?}", packets);
+
+    // for (x, y) in packets {
+    //     println!("{:?}", x);
+    //     println!("{:?}", y);
+    //     println!("");
+    // }
     1
 }
 
