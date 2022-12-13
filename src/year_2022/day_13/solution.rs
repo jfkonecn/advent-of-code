959ebeedc30abfaeff1fe570902805b6a40f7bef
John <jfkonecn@gmail.com>
2022-12-13 12:30:47 -0500

    a

--- src/year_2022/day_13/solution.rs
+++ src/year_2022/day_13/solution.rs
@@ -4,136 +4,68 @@ use std::cmp::*;
 use std::collections::*;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-enum NodeType {
-    Start,
-    Middle,
-    End,
+struct PacketList {
+    list: Vec<PacketIndex>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct Node {
-    row: usize,
-    col: usize,
-    height: usize,
-    distance_to_end: usize,
-    node_type: NodeType,
+enum PacketIndex {
+    List(PacketList),
+    Num(usize),
 }
 
-#[derive(Debug, Clone)]
-struct Graph {
-    nodes: HashMap<(usize, usize), Node>,
-    start: Node,
-    end: Node,
-}
-
-impl From<String> for Graph {
-    fn from(str: String) -> Self {
-        let str_lines = str.split('\n').filter(|x| !x.is_empty());
-        let mut raw_start = (0, 0);
-        let mut raw_end = (0, 0);
-        let mut raw_nodes = HashMap::new();
-        for (row, str) in str_lines.enumerate() {
-            for (col, c_num) in str.chars().map(|x| x as usize).enumerate() {
-                let (node_type, height) = if 'S' as usize == c_num {
-                    raw_start = (row, col);
-                    (NodeType::Start, 1)
-                } else if 'E' as usize == c_num {
-                    raw_end = (row, col);
-                    (NodeType::End, 26)
-                } else {
-                    (NodeType::Middle, c_num - 'a' as usize + 1)
-                };
-                raw_nodes.insert((row, col), (height, node_type));
+impl From<&str> for PacketList {
+    fn from(str: &str) -> Self {
+        let list = vec![];
+        let mut stack = vec![];
+        let char_iter = str.chars();
+        while let Some(c) = char_iter.next() {
+            let c = if c.is_numeric() {
+                let str = c.to_string().to_owned();
+                loop {
+                    let c = char_iter.next().unwrap();
+                    if c.is_numeric() {
+                        str.push(c);
+                    } else {
+                        let num: usize = str.parse().unwrap();
+                        stack.last().unwrap().push(PacketIndex::Num(num));
+                        break c;
+                    }
+                }
+            } else {
+                c
+            };
+            if c == '[' {
+                stack.push(vec![]);
+            } else if c == ']' {
+                list.push(PacketIndex::List(PacketList {
+                    list: stack.pop().unwrap(),
+                }));
             }
         }
-
-        let mut nodes = HashMap::new();
-
-        for ((row, col), (height, node_type)) in &raw_nodes {
-            let (row, col) = (*row, *col);
-            let (end_row, end_col) = raw_end;
-            let distance_to_end = f64::sqrt(
-                f64::powi(row.abs_diff(end_row) as f64, 2)
-                    + f64::powi(col.abs_diff(end_col) as f64, 2),
-            );
-            let distance_to_end = (distance_to_end * 1000f64) as usize;
-            nodes.insert(
-                (row, col),
-                Node {
-                    col,
-                    row,
-                    distance_to_end,
-                    height: *height,
-                    node_type: node_type.clone(),
-                },
-            );
-        }
-        Graph {
-            start: nodes.get(&raw_start).unwrap().clone(),
-            end: nodes.get(&raw_end).unwrap().clone(),
-            nodes,
-        }
+        PacketList { list }
     }
 }
-fn shortest_path(graph: &Graph, start: &Node) -> Option<(Vec<Node>, usize)> {
-    pathfinding::directed::dijkstra::dijkstra(
-        // pathfinding::directed::astar::astar(
-        start,
-        |node: &Node| {
-            let node = node.clone();
-            let mut edges = vec![(node.row + 1, node.col), (node.row, node.col + 1)];
-            if node.row > 0 {
-                edges.push((node.row - 1, node.col));
-            }
-            if node.col > 0 {
-                edges.push((node.row, node.col - 1));
-            }
-            let edges = edges
-                .iter()
-                .filter_map(move |key| {
-                    let cur_node_id_opt = graph.nodes.get(&key);
-                    if let Some(_) = cur_node_id_opt {
-                        let cur_node = graph.nodes.get(&key).unwrap();
-                        if cur_node.height.abs_diff(node.height) <= 1
-                            || cur_node.height < node.height
-                        {
-                            Some((cur_node.clone(), 100000))
-                        } else {
-                            None
-                        }
-                    } else {
-                        None
-                    }
-                })
-                .collect_vec();
-            edges
-        },
-        |x| x.node_type == NodeType::End,
-    )
+
+fn parse_packets(str: String) -> Vec<(PacketList, PacketList)> {
+    str.split("\n\n")
+        .map(|x| {
+            let iter = x.split('\n');
+            let first = iter.next().unwrap().into();
+            let second = iter.next().unwrap().into();
+            (first, second)
+        })
+        .collect_vec()
 }
 
 pub fn solution_1(file_contents: String) -> usize {
-    let graph = Graph::from(file_contents);
-    let (vec, _) = shortest_path(&graph, &graph.start).unwrap();
-    vec.len() - 1
+    let packets = parse_packets(file_contents);
+    println!("{:#?}", packets);
+    1
 }
 
 pub fn solution_2(file_contents: String) -> usize {
-    let graph = Graph::from(file_contents);
-    let shortest = graph
-        .nodes
-        .iter()
-        .filter(|(_, x)| x.height == 1)
-        .map(|(_, x)| x.clone());
-    let mut dist = usize::MAX;
-    for short in shortest {
-        let opt = shortest_path(&graph, &short);
-
-        if let Some((vec, _)) = opt {
-            dist = dist.min(vec.len() - 1);
-        }
-    }
-    dist
+    1
 }
 
 challenge_test_suite!(
