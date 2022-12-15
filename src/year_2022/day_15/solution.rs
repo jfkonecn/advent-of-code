d9a7cf07aaaf1f154f7e8c46c2e63c6d18a0bf47
John <jfkonecn@gmail.com>
2022-12-15 08:28:10 -0500

    a

--- src/year_2022/day_15/solution.rs
+++ src/year_2022/day_15/solution.rs
@@ -4,91 +4,109 @@ use std::cmp::*;
 use std::collections::*;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-enum MapType {
-    Empty,
-    Sensor,
-    Beacon,
-}
-
 struct SBPair {
     signal_point: (isize, isize),
     beacon_point: (isize, isize),
 }
 
 impl SBPair {
-    fn get_empty_points(&self, row: isize) -> HashSet<(isize, isize)> {
-        let set = HashSet::new();
+    fn get_empty_points(&self, y: isize) -> HashSet<(isize, isize)> {
+        let mut set = HashSet::new();
+        let (s_x, s_y) = self.signal_point;
+        let (b_x, b_y) = self.beacon_point;
+        let distance = (s_x.abs_diff(b_x) + s_y.abs_diff(b_y)) as isize;
+        let x_distance = distance - s_y.abs_diff(y) as isize;
+        for x in s_x - x_distance..s_x + x_distance + 1 {
+            if !(x == s_x && y == s_y) && !(x == b_x && y == b_y) {
+                set.insert((x, y));
+            }
+        }
         set
     }
 }
 
-fn parse_map(str: String) -> HashMap<(isize, isize), MapType> {
-    let mut hash_map = HashMap::new();
-    str.split("\n").filter(|x| !x.is_empty()).for_each(|x| {
-        let mut iter = x.split("=");
-        iter.next();
-        let s_x: isize = iter
-            .next()
-            .unwrap()
-            .split(',')
-            .next()
-            .unwrap()
-            .trim()
-            .parse()
-            .unwrap();
-        let s_y: isize = iter
-            .next()
-            .unwrap()
-            .split(':')
-            .next()
-            .unwrap()
-            .trim()
-            .parse()
-            .unwrap();
-        let b_x: isize = iter
-            .next()
-            .unwrap()
-            .split(',')
-            .next()
-            .unwrap()
-            .trim()
-            .parse()
-            .unwrap();
-        let b_y: isize = iter
-            .next()
-            .unwrap()
-            .split(':')
-            .next()
-            .unwrap()
-            .trim()
-            .parse()
-            .unwrap();
-        println!("--------------");
-        // println!("Sensor ({}, {}) - Beacon ({}, {})", s_x, s_y, b_x, b_y);
-        let distance = (s_x.abs_diff(b_x) + s_y.abs_diff(b_y)) as isize;
-        for y in s_y - distance..s_y + distance + 1 {
-            let x_distance = distance - s_y.abs_diff(y) as isize;
-            for x in s_x - x_distance..s_x + x_distance + 1 {
-                let map_type = if x == s_x && y == s_y {
-                    MapType::Sensor
-                } else if x == b_x && y == b_y {
-                    MapType::Beacon
-                } else {
-                    MapType::Empty
-                };
-                println!("({}, {}) - {:?}", x, y, map_type);
-                hash_map.insert((x, y), map_type);
+fn parse_map(str: String) -> Vec<SBPair> {
+    str.split("\n")
+        .filter(|x| !x.is_empty())
+        .map(|x| {
+            let mut iter = x.split("=");
+            iter.next();
+            let s_x: isize = iter
+                .next()
+                .unwrap()
+                .split(',')
+                .next()
+                .unwrap()
+                .trim()
+                .parse()
+                .unwrap();
+            let s_y: isize = iter
+                .next()
+                .unwrap()
+                .split(':')
+                .next()
+                .unwrap()
+                .trim()
+                .parse()
+                .unwrap();
+            let b_x: isize = iter
+                .next()
+                .unwrap()
+                .split(',')
+                .next()
+                .unwrap()
+                .trim()
+                .parse()
+                .unwrap();
+            let b_y: isize = iter
+                .next()
+                .unwrap()
+                .split(':')
+                .next()
+                .unwrap()
+                .trim()
+                .parse()
+                .unwrap();
+            println!("--------------");
+            println!("Sensor ({}, {}) - Beacon ({}, {})", s_x, s_y, b_x, b_y);
+            // let distance = (s_x.abs_diff(b_x) + s_y.abs_diff(b_y)) as isize;
+            // for y in s_y - distance..s_y + distance + 1 {
+            //     let x_distance = distance - s_y.abs_diff(y) as isize;
+            //     for x in s_x - x_distance..s_x + x_distance + 1 {
+            //         let map_type = if x == s_x && y == s_y {
+            //             MapType::Sensor
+            //         } else if x == b_x && y == b_y {
+            //             MapType::Beacon
+            //         } else {
+            //             MapType::Empty
+            //         };
+            //         println!("({}, {}) - {:?}", x, y, map_type);
+            //         hash_map.insert((x, y), map_type);
+            //     }
+            // }
+            println!("--------------");
+            SBPair {
+                signal_point: (s_x, s_y),
+                beacon_point: (b_x, b_y),
             }
-        }
-        println!("--------------");
-    });
-    hash_map
+        })
+        .collect_vec()
 }
 
 pub fn solution_1(file_contents: String) -> usize {
-    let mut map = parse_map(file_contents);
-    println!("{:?}", map);
-    1
+    let pairs = parse_map(file_contents);
+    println!("{:?}", pairs);
+    let y = 2000000;
+    let set = pairs
+        .iter()
+        .map(|x| x.get_empty_points(y))
+        .fold(HashSet::new(), |mut acc, set| {
+            for x in set {
+                acc.insert(x);
+            }
+            acc
+        });
+    set.len()
 }
 
 pub fn solution_2(file_contents: String) -> usize {
@@ -99,8 +117,8 @@ pub fn solution_2(file_contents: String) -> usize {
 
 challenge_test_suite!(
     solution_1,
-    1,
-    1,
+    0,
+    5878678,
     solution_2,
     1,
     1,
