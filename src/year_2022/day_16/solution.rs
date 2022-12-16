db753af18c86431a60584f457a55fccbbbf83f9e
John <jfkonecn@gmail.com>
2022-12-16 08:29:50 -0500

    a

--- src/year_2022/day_16/solution.rs
+++ src/year_2022/day_16/solution.rs
@@ -10,6 +10,20 @@ struct RawValve {
     tunnels: Vec<String>,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Tunnel {
+    to: String,
+    flow_rate: usize,
+    distance: usize,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Valve {
+    id: String,
+    flow_rate: usize,
+    tunnels: Vec<Tunnel>,
+}
+
 impl From<&str> for RawValve {
     fn from(str: &str) -> Self {
         let id = str[6..8].to_owned();
@@ -40,15 +54,55 @@ impl From<&str> for RawValve {
     }
 }
 
-fn parse_valves(file_contents: String) -> Vec<RawValve> {
+fn parse_raw_valves(file_contents: String) -> Vec<RawValve> {
     file_contents
         .split("\n")
         .filter(|x| !x.is_empty())
         .map_into()
         .collect_vec()
 }
+
+fn get_tunnels(raw_valve: &RawValve, graph: &Vec<RawValve>) -> Vec<Tunnel> {
+    let mut visited = HashMap::new();
+    let mut stack = vec![raw_valve];
+
+    let mut cur_distance = 0;
+    while let Some(cur_valve) = stack.pop() {
+        visited.insert(cur_valve.id.clone(), (cur_distance, cur_valve.flow_rate));
+        cur_distance += 1;
+        for tunnel_id in cur_valve.tunnels.iter() {
+            if let Some(mut x) = visited.get_mut(tunnel_id) {
+                x.0 = x.0.min(cur_distance);
+            } else {
+                stack.push(graph.iter().find(|x| &x.id == tunnel_id).unwrap())
+            }
+        }
+    }
+    visited
+        .into_iter()
+        .map(|(key, (distance, flow_rate))| Tunnel {
+            distance,
+            flow_rate,
+            to: key,
+        })
+        .filter(|x| x.distance > 0 && x.flow_rate > 0)
+        .collect_vec()
+}
+
+fn to_valves(raw_valves: &Vec<RawValve>) -> Vec<Valve> {
+    raw_valves
+        .iter()
+        .map(|x| Valve {
+            flow_rate: x.flow_rate,
+            id: x.id.clone(),
+            tunnels: get_tunnels(x, raw_valves),
+        })
+        .collect_vec()
+}
+
 pub fn solution_1(file_contents: String) -> usize {
-    let valves = parse_valves(file_contents);
+    let raw_valves = parse_raw_valves(file_contents);
+    let valves = to_valves(&raw_valves);
     println!("{:?}", valves);
     0
 }
